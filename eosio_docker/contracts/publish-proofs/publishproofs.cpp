f1612f2f228443bab9dec5d239d1ddd0d77d3c6f
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-09-22 19:34:29 +0100

    Fixing compilation errors

--- eosio_docker/contracts/publish-proofs/publishproofs.cpp
+++ eosio_docker/contracts/publish-proofs/publishproofs.cpp
@@ -15,35 +15,39 @@ class publishproofs : public eosio::contract {
       // primary key
       auto primary_key() const { return prim_key; }
       // secondary key: user
-      uint64_t get_by_proof() const { return proof; }
+      // uint64_t get_by_proof() const { return proof; }
     };
 
     // create a multi-index table and support secondary key
-    typedef eosio::multi_index< N(proofstruct), proofstruct,
-      indexed_by< N(getbyproof), const_mem_fun<proofstruct, std::string, &proofstruct::get_by_proof> >
-      > prooftable;
+    // typedef eosio::multi_index< N(proofstruct), proofstruct,
+    //   indexed_by< N(getbyproof), const_mem_fun<proofstruct, std::string, &proofstruct::get_by_proof> >
+    //   > prooftable;
+    typedef eosio::multi_index<N(proofstruct), proofstruct> proofs;
+    proofs _proofs;
 
   public:
     using contract::contract;
+
+    publishproofs(account_name self):contract(self), _proofs(self, self){}
 
     /// @abi action
     void publish( account_name _bank, std::string& _proof ) {
       // to sign the action with the given account
       require_auth( _bank );
 
-      prooftable obj(_self, _self); // code, scope
+      // prooftable obj(_self, _self); // code, scope
 
-      obj.emplace( _self, [&]( auto& foobar ) {
-        foobar.prim_key = obj.available_primary_key();
-        foobar.bank = _bank;
-        foobar.proof = _proof;
-        foobar.timestamp = now();
+      _proofs.emplace( _self, [&]( auto& proof ) {
+        proof.prim_key = _proofs.available_primary_key();
+        proof.bank = _bank;
+        proof.proof = _proof;
+        proof.timestamp = now();
       });
     }
 
     /// @abi action
-    account_name verify(std::string& _proof) {
-      prooftable obj(_self, _self); // code, scope
+    void verify(std::string& _proof) {
+      // prooftable obj(_self, _self); // code, scope
 
       // TODO: return account_name of bank mathcing the proof
     }
@@ -51,4 +55,4 @@ class publishproofs : public eosio::contract {
 };
 
 // specify the contract name, and export a public action: update
-EOSIO_ABI( publishproofs, (publish) )
+EOSIO_ABI( publishproofs, (publish)(verify) )
