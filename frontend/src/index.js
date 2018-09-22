0e6d428d5ef8b79e5dbdf835c5ce30c4daea9f2a
Linus Unnebäck <linus@folkdatorn.se>
2018-09-22 22:17:26 +0100

    Add routing to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Publish from './pages/publish'
 import Verify from './pages/verify'
-import Bank from './pages/bank/bank';
+import Bank from './pages/bank/bank'
 
-// ReactDOM.render(<Bank />, document.getElementById('root'));
-ReactDOM.render(<Publish />, document.getElementById('root'))
-// ReactDOM.render(<Verify />, document.getElementById('root'))
+const Menu = () => (
+  <ul>
+    <li><Link to='/bank'>Bank</Link></li>
+    <li><Link to='/publish'>Publish Proof</Link></li>
+    <li><Link to='/verify'>Verify Proof</Link></li>
+  </ul>
+)
+
+const App = () => (
+  <div>
+    <Route exact path='/' component={Menu} />
+    <Route exact path='/bank' component={Bank} />
+    <Route exact path='/publish' component={Publish} />
+    <Route exact path='/verify' component={Verify} />
+  </div>
+)
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
