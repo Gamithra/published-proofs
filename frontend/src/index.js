74e9dcf566ca62b4ac76a39ef6d10a0610bc4058
Dhilip Buddha <dhilip.buddha@blackrock.com>
2018-09-22 22:49:41 +0100

    HSBC

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Publish from './pages/publish'
-import Bank from './pages/bank/bank';
+// import Publish from './pages/publish'
+// import Bank from './pages/bank/bank';
+import Hsbc from './pages/hsbc/hsbc';
 
-// ReactDOM.render(<Bank />, document.getElementById('root'));
-ReactDOM.render(<Publish />, document.getElementById('root'))
+ReactDOM.render(<Hsbc />, document.getElementById('root'));
+// ReactDOM.render(<Publish />, document.getElementById('root'))
---
+++ frontend/src/pages/hsbc/hsbc.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./hsbc.css";
+import Publish from '../publish';
+
+export class Hsbc extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {showPublish: false};
+  }
+
+  handleClick = () => {
+    console.log('click');  
+    this.setState({
+      showPublish: true
+    });
+  }
+
+  render() {
+    const publishComponent = (
+      <div id="publish-component-container">
+          <div id="publish-container">
+            <Publish></Publish>
+          </div>
+      </div>
+    );
+
+    return (
+      <div id="main-container">
+        {this.state.showPublish && publishComponent}
+        <div id="hsbc-container">
+          <button onClick={this.handleClick} className="send-info">
+            Send Info
+            <span className="send-icon"></span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hsbc;
