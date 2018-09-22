1db9fbb90bdeb7d2dc3d451f3c9a658d2221073d
Linus Unnebäck <linus@folkdatorn.se>
2018-09-22 22:01:38 +0100

    Add a frontend for validating proofs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Publish from './pages/publish'
+import Verify from './pages/verify'
 import Bank from './pages/bank/bank';
 
 // ReactDOM.render(<Bank />, document.getElementById('root'));
 ReactDOM.render(<Publish />, document.getElementById('root'))
+// ReactDOM.render(<Verify />, document.getElementById('root'))
