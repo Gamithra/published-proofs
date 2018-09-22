c6257128142ccd4843c92a80514e7d6d65ed3320
Linus Unnebäck <linus@folkdatorn.se>
2018-09-22 20:09:59 +0100

    Add first iteration of verifyProof

--- frontend/src/lib/publishproof.js
+++ frontend/src/lib/publishproof.js
@@ -51,3 +51,35 @@ export async function publishProof (userId, amount, dateRange) {
 
   console.log(result)
 }
+
+/**
+ * @param {string} userId
+ * @param {string} amount
+ * @param {string} dateRange
+ * @returns {Promise<null | string>} - Either the account id of the bank, or null if the proof wasn't valid
+ */
+export async function verifyProof (userId, amount, dateRange) {
+  const eos = Eos({ keyProvider: privateKey })
+
+  const proof = sha256(`${userId} had a minimum balance of ${amount} during ${dateRange}`)
+
+  const result = await eos.getTableRows({
+    json: true,
+    code: 'publishproofs',
+    scope: 'publishproofs',
+    table: '_proofs',
+    tableKey: 'proof',
+    lower_bound: proof,
+    limit: 1
+  })
+
+  // if (result.more) {
+  //   throw new Error('More than one result for proof')
+  // }
+
+  if (result.rows.length === 0) {
+    return null
+  }
+
+  return result.rows[0].bank
+}
