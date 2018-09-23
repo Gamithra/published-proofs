d45a3e72d3d62c8d7605e8f238440ea7b9a2a074
Linus Unnebäck <linus@folkdatorn.se>
2018-09-23 10:21:23 +0100

    Add loading and published state to publish popup

--- frontend/src/pages/publish.jsx
+++ frontend/src/pages/publish.jsx
@@ -1,80 +1,98 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-// material-ui dependencies
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
-import Button from "@material-ui/core/Button";
-import './publish.css';
-import logo from './ver1b-white.png';
+import './publish.css'
+import logo from './ver1b-white.png'
 
-import { publishProof } from "../lib/publishproof";
+import { publishProof } from '../lib/publishproof'
 
 export default class Publish extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   balance: 10000,
-    //   months: 3
-    // }
-    this.handleFormEvent = this.handleFormEvent.bind(this);
+  constructor (props) {
+    super(props)
+    this.state = { loading: false, published: false }
+    this.handleFormEvent = this.handleFormEvent.bind(this)
   }
 
-  // generic function to handle form events (e.g. "submit" / "reset")
   // push transactions to the blockchain by using eosjs
-  async handleFormEvent(event) {
+  async handleFormEvent (event) {
     // stop default behaviour
-    event.preventDefault();
+    event.preventDefault()
+
+    this.setState({ loading: true })
 
     // collect form data
-    let balance = event.target.balance.value;
-    let months = event.target.months.value;
+    let balance = event.target.balance.value
+    let months = event.target.months.value
 
     // publish proof
-    await publishProof("Mr. Linus", balance, `last ${months} months`);
+    await publishProof('Linus Unnebäck', balance, `last ${months} months`)
+
+    this.setState({ loading: false, published: true })
   }
 
-  render() {
+  renderForm () {
     return (
-      <div className="publish-component">
-      {/*<h2 className="header">Publish Proof</h2>*/}
-         <div className="company-name">
-                <img src={logo} alt="logo" className="logo" />
-         </div>
-        <form onSubmit={this.handleFormEvent} className="publish-form">
-       
-          During the last
-          
-          <p> <input
-            name="months"
-            type="number"
-            width="30"
-          /> months, </p> 
-          I've maintained an average balance of at least
-          <p> <input
-            name="balance"
-            type="number"
-            autoComplete="off"
-            label="Balance"
-            margin="normal"
-            width="5"
-            step="500"
-            min="500"
-          /> GBP. </p>
+      <form onSubmit={this.handleFormEvent} className='publish-form'>
+        During the last
 
-          
-          Recipient's e-mail address: <br />
-          <input name="recipient" />
-          <button className="submit-button" type="submit">
-            Request proof
-          </button>
-          
-        </form>
-          <p className="footer"> Powered by <span className="underline">Sanna</span> </p>
+        <p>
+          <input
+            name='months'
+            type='number'
+            width='30'
+          />
+          months,
+        </p>
+        I've maintained an average balance of at least
+        <p>
+          <input
+            name='balance'
+            type='number'
+            autoComplete='off'
+            width='5'
+            step='500'
+            min='500'
+          /> GBP.
+        </p>
+
+        Recipient's e-mail address: <br />
+        <input name='recipient' />
+        <button className='submit-button' type='submit'>
+          Request proof
+        </button>
+
+      </form>
+    )
+  }
+
+  renderLoading () {
+    return (
+      <p>
+        Publishing proof to the blockchain...
+      </p>
+    )
+  }
+
+  renderPublished () {
+    return (
+      <p>
+        Proof successfully published to the blockchain!
+      </p>
+    )
+  }
+
+  render () {
+    const { published, loading } = this.state
+
+    return (
+      <div className='publish-component'>
+        <div className='company-name'>
+          <img src={logo} alt='logo' className='logo' />
+        </div>
+
+        {published ? this.renderPublished() : loading ? this.renderLoading() : this.renderForm()}
+
+        <p className='footer'> Powered by <span className='underline'>Sanna</span> </p>
       </div>
-    );
+    )
   }
 }
