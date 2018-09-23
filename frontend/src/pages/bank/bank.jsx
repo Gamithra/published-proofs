47923a21bd93e1101eba10dc0d6bb3754a22c1e3
Linus Unnebäck <linus@folkdatorn.se>
2018-09-23 10:10:20 +0100

    Read token from query-string to add row

--- frontend/src/pages/bank/bank.jsx
+++ frontend/src/pages/bank/bank.jsx
@@ -1,72 +1,83 @@
-import React, { Component } from "react";
-import "./bank.css";
-import logo from '../ver1b-white.png';
+import React, { Component } from 'react'
+import './bank.css'
+import { withRouter } from 'react-router-dom'
 
-export class Bank extends Component {
-  constructor(props) {
-    super(props);
+import logo from '../ver1b-white.png'
+
+class Bank extends Component {
+  getTransactions () {
+    const params = new URLSearchParams(this.props.location.search)
+    const token = params.get('token')
+
+    const extra = {
+      vendor: 'Bank of Foobar',
+      date: '2018-09-23',
+      amount: 40000
+    }
+
+    const base = [
+      {
+        vendor: 'Lorem ipsum dolor',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Consectetur adipiscing elit',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Proin lectus sem',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Malesuada sed ipsum',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Lacinia fermentum libero',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Commodo nulla tincidunt ',
+        date: '2018-09-22',
+        amount: 20
+      },
+      {
+        vendor: 'Vivamus lacinia fringilla',
+        date: '2018-09-22',
+        amount: 20
+      }
+    ]
+
+    return (token ? [extra, ...base] : base)
   }
-  getTransactions() {
-    return [
-        {
-          vendor: "Lorem ipsum dolor",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Consectetur adipiscing elit",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Proin lectus sem",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Malesuada sed ipsum",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Lacinia fermentum libero",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Commodo nulla tincidunt ",
-          date: "2018-09-22",
-          amount: 20
-        },
-        {
-          vendor: "Vivamus lacinia fringilla",
-          date: "2018-09-22",
-          amount: 20
-        },
-    ];
-  }
-  render() {
-    const transactions = this.getTransactions();
-    const currency = "$";
+
+  render () {
+    const transactions = this.getTransactions()
+    const currency = '$'
 
     return (
-      <div id="bank-container">
-        <div className="header">
-          <div className="container">
-            <div className="bank-name">
-                <img src={logo} alt="logo" className="logo" />
+      <div id='bank-container'>
+        <div className='header'>
+          <div className='container'>
+            <div className='bank-name'>
+              <img src={logo} alt='logo' className='logo' />
                 Sanna
             </div>
-            <div className="actions">
+            <div className='actions'>
                Hello, Dan!
-               <br/>
-               <button className="link"> Log out </button>
+              <br />
+              <button className='link'> Log out </button>
             </div>
           </div>
         </div>
-        <div className="content">
-          <div className="container">
-            <table cellPadding="0" cellSpacing="0">
+        <div className='content'>
+          <div className='container'>
+            <table cellPadding='0' cellSpacing='0'>
               <thead>
                 <tr>
                   <th>User</th>
@@ -74,7 +85,7 @@ export class Bank extends Component {
                   <th>Bank</th>
                   <th>Amount</th>
                   <th>Months</th>
-                  <th className="align-right">Success</th>
+                  <th className='align-right'>Success</th>
                 </tr>
               </thead>
               <tbody>
@@ -85,7 +96,7 @@ export class Bank extends Component {
                     <td>Nice Bank</td>
                     <td>{currency}{tx.amount.toFixed(2)}</td>
                     <td> 6 </td>
-                    <td className="align-right">Yes</td>
+                    <td className='align-right'>Yes</td>
                   </tr>
                 ))}
               </tbody>
@@ -93,8 +104,8 @@ export class Bank extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Bank;
+export default withRouter(Bank)
